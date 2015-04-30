22ec7ac8e4e7641bfcb36f192c958bbcc3e0ada1
Sichais <virshadoe@gmail.com>
2015-04-29 21:47:43 -0500

    Initial Commit

---
+++ ArtificialEmotionC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ArtificialEmotionC
+//
+//  Created by Zane Lassiter on 4/29/15.
+//  Copyright (c) 2015 Zane Lassiter. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
